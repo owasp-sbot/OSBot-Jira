7eb60f5442edf6536efe1e376ed17627917a7f79
Dinis Cruz <dinis.cruz@owasp.org>
2019-07-09 05:21:49 +0100

    Improved Slack Blocks API

--- osbot_jira/api/slack/API_Slack_Blocks.py
+++ osbot_jira/api/slack/API_Slack_Blocks.py
@@ -1,28 +1,144 @@
 ## Todo: refactor to a new OSBot_Slack (which also has the other classes currently in pbx_gs-python_utils
 from pbx_gs_python_utils.utils.slack.API_Slack import API_Slack
 
+## todo: move to separate file (and refactor common methods
+class Layout_Action:
+    def __init__(self, block_id, blocks):
+        self.block_id = block_id
+        self.blocks = blocks
+        self.elements = []
+
+    def add_button(self, text, action_id=None, url=None, value=None, style=None,confirm=None):
+        button = {'type'     : "button"  ,
+                  'text'     : {"type": 'plain_text', "text": text}}
+        if action_id: button['action_id'] = action_id;
+        if url      : button['url'      ] = url
+        if style    : button['style'    ] = style
+        if value    : button['value'    ] = value
+        if confirm  : button['confirm'  ] = confirm
+
+        if not action_id: button['action_id'] = text
+
+        self.elements.append(button)
+        return self
+
+    def add_image(self):
+        self.elements.append({ "type": "image",
+                               "image_url": image_url,
+            "alt_text": "Multiple cute kittens"
+        })
+
+    def render(self):
+        self.blocks.append({"type": "actions",
+                            "block_id": self.block_id,
+                            "elements": self.elements})
+        return self
+
 class API_Slack_Blocks:
     def __init__(self, text=None, callback_id=None, fallback=None):
-        self.text        = text
-        self.callback_id = callback_id
-        self.fallback    = fallback
-        self.blocks      = []
-
+        self.text            = text
+        self.callback_id     = callback_id
+        self.fallback        = fallback
+        self.attachments     = []
+        self.blocks          = []
+        self.as_user         = False
+        self.icon_emoji      = None
+        self.icon_url        = None
+        self.mrkdwn          = True
+        self.link_names      = False
+        self.parse           = 'none'
+        self.username        = None
+        self.kwargs          = None
+        self.reply_broadcast = False
+        self.thread_ts       = None
+        self.unfurl_links    = False
+        self.unfurl_media    = False
     # Helper methods
 
     def send_message(self,channel=None, team_id=None):                      # needs to move to the main dedicated lambda function
         api_slack = API_Slack(channel=channel, team_id=team_id)
         if  channel and team_id:                                            # to help with testing
-            return api_slack.slack.api_call("chat.postMessage", channel=api_slack.channel, text=self.text, blocks=self.blocks)
+            self.kwargs = {
+                        'channel'        : api_slack.channel   ,
+                        'text'           : self.text           ,
+                        'as_user'        : self.as_user        ,
+                        'attachments'    : self.attachments    ,
+                        'blocks'         : self.blocks         ,
+                        'icon_emoji'     : self.icon_emoji     ,
+                        'icon_url'       : self.icon_url       ,
+                        'link_names'     : self.link_names     ,
+                        'mrkdwn'         : self.mrkdwn         ,
+                        'parse'          : self.parse          ,
+                        'reply_broadcast': self.reply_broadcast,
+                        'thread_ts'      : self.thread_ts      ,
+                        'unfurl_links'   : self.unfurl_links   ,
+                        'unfurl_media'   : self.unfurl_media   ,
+                        'username'       : self.username
+                     }
+            return api_slack.slack.api_call("chat.postMessage", **self.kwargs)
         else:
             return self.text, self.blocks
 
-    def set_text(self, text):
-        self.text = text
-        return self
+    def set_as_user        (self, value): self.as_user         = value ; return self
+    def set_icon_emoji     (self, value): self.icon_emoji      = value ; return self
+    def set_icon_url       (self, value): self.icon_url        = value ; return self
+    def set_text           (self, value): self.text            = value ; return self
+    def set_link_names     (self, value): self.link_names      = value ; return self
+    def set_mrkdwn         (self, value): self.mrkdwn          = value ; return self
+    def set_parse          (self, value): self.parse           = value ; return self
+    def set_reply_broadcast(self, value): self.reply_broadcast = value ; return self
+    def set_thread_ts      (self, value): self.thread_ts       = value ; return self
+    def set_unfurl_links   (self, value): self.unfurl_links    = value ; return self
+    def set_unfurl_media   (self, value): self.unfurl_media    = value ; return self
+    def set_username       (self, value): self.username        = value ; return self
 
-    # add blocks
+    # add layouts
+
+    def add_layout_actions(self, block_id):
+
+        actions = Layout_Action(block_id, self.blocks)
+
+        return actions
+
+    # add element blocks
+
+    def add_button(self, text, value=None, action_id=None, button_type="plain_text"):
+        self.blocks.append({
+		"type": "actions",
+		"block_id": "actionblock789",
+		"elements": [
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Primary Button"
+				},
+				"style": "primary",
+				"value": "click_me_456"
+			},
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Link Button"
+				},
+				"url": "https://api.slack.com/block-kit"
+			}
+		]
+	})
+     #   self.blocks.append({"type"     : "button",
+     #                       "text"     : { "type": button_type, "text": text },
+     #                       "value"    : value,
+     #                       "action_id": action_id })
 
     def add_divider(self):
         self.blocks.append({"type": "divider"})
         return self
+
+    def add_image(self, image_url, alt_text):
+        self.blocks.append({ "type": "image", "image_url": image_url, "alt_text":  alt_text})
+
+    def add_attachment(self,attachment):
+        self.attachments.append(attachment)
+        return self
+    #def add_text
--- tests/unit/api/slack/test_API_Slack_Blocks.py
+++ tests/unit/api/slack/test_API_Slack_Blocks.py
@@ -11,6 +11,8 @@ class test_API_Slack_Blocks(TestCase):
         self.result  = None
         self.channel = 'DDKUZTK6X'
         self.team_id = 'T7F3AUXGV'
+        #self.team_id = 'T0SDK1RA8'
+        #self.channel = 'C77N26S9K' #, 'DJXSMSQH3' #''GJKBUSLJE' #'DG30MH0KV'
 
     def tearDown(self):
         self.send_message()
@@ -18,10 +20,115 @@ class test_API_Slack_Blocks(TestCase):
             Dev.pprint(self.result)
 
     def send_message(self):
-        assert self.api.send_message(self.channel, self.team_id).get('ok') is True
+        result = self.api.send_message(self.channel, self.team_id)
+        if result.get('ok') is False:
+            self.result = result
+        else:
+            assert result.get('ok') is True
+
+    def test_set_icon_emoji(self):
+        self.api.set_text('before 1st test').set_as_user(True)      # this seems to reset the use of emojies in Slack
+        self.send_message()
+        self.api.set_text('with chart_with_upwards_trend').set_as_user(False).set_icon_emoji(':chart_with_upwards_trend:')
+        self.send_message()
+        self.api.set_text('before 2nd test').set_as_user(True)
+        self.send_message()
+        self.api.set_text('with point_right').set_as_user(False).set_icon_emoji(':point_right:')
+
+    def test_set_icon_url(self):
+        self.api.set_text('before 1st test').set_as_user(True)  # this seems to reset the use of emojies in Slack
+        self.send_message()
+        self.api.set_text('with icon url').set_as_user(False).set_icon_url('http://lorempixel.com/48/48')
+
+    def test_set_link_names(self): # todo: not working as expected
+        names = '@dinis.cruz  | #general'
+        self.api.set_text('with set_link_names = False: ' + names)
+        self.send_message()
+        self.api.set_link_names(True).set_text('now set to True: ' + names)
+        self.send_message()
+        self.api.set_link_names(False).set_text('back to False: ' + names)
+
 
     def test_set_text(self):
         self.api.set_text('this is an text using the api that supports blocks')
 
+    def test_set_mrkdwn(self): # todo: not working as expected
+        text = 'this `is` some *markdown* formating  \n - abc \n -xyz'
+        self.api.set_text(text).set_mrkdwn(False)
+        self.send_message()
+        assert self.api.kwargs.get('mrkdwn') is False
+        self.api.set_text(text).set_mrkdwn(True)
+        self.send_message()
+        assert self.api.kwargs.get('mrkdwn') is True
+
+    def test_set_parse(self): # todo: not working as expected
+        text = '(for parse) this `is` some *markdown* formating  \n - abc \n -xyz'
+        self.api.set_text(text)
+        self.send_message()     # defaults to none
+        self.api.set_parse('full')
+
+    def test_set_username(self):
+        self.api.set_text('This is from an not existing user')   \
+                .set_as_user(False)                              \
+                .set_icon_url('http://lorempixel.com/48/48')     \
+                .set_username('User XYZ')
+
+    # add layouts
+
+    def test_add_layout_actions(self):
+        self.api.set_text('with action layouts')
+        actions = self.api.add_layout_actions("actionblock789")
+        actions.add_button('first button', 'action_id_1')
+        actions.add_button('primary'  , style='primary')
+        actions.add_button('danger', style='danger')
+        actions.add_button('bbx',  value='abc', url='https://news.bbc.co.uk',
+                                   confirm={
+                                              "title": {
+                                                  "type": "plain_text",
+                                                  "text": "Are you sure?"
+                                              },
+                                              "text": {
+                                                  "type": "mrkdwn",
+                                                  "text": "Wouldn't you prefer a good game of _chess_?"
+                                              },
+                                              "confirm": {
+                                                  "type": "plain_text",
+                                                  "text": "Do it"
+                                              },
+                                              "deny": {
+                                                  "type": "plain_text",
+                                                  "text": "Stop, I've changed my mind!"
+                                              }
+                                            })
+        actions.add_button('text 123 ','action_id_2')
+        actions.add_button('another button')
+        actions.add_button('6th button')
+        actions.add_button('8th button')
+        actions.add_button('9th button')
+        actions.add_button('10th button')
+        actions.add_button('11th button')
+        actions.add_button('12th button')
+        actions.render()
+
+    def test_add_layout_actions__images(self):
+        actions = self.api.add_layout_actions("action_block")
+        actions.add_button('an button', style='primary')
+        actions.add_image()
+        actions.render()
+
+
+
+    # add element blocks
+
+    def test_add_attachments(self):
+        self.api.set_text('an text').add_attachment({"pretext": "pre-hello", "text": "text-world"})
+
+
+    def test_add_button(self):
+        self.api.add_button("Click Me", value='click_me_123', action_id="button")
+
     def test_add_divider(self):
-        self.api.set_text('with divider').add_divider()
+        self.api.add_divider()
+
+    def test_add_image(self):
+        self.api.add_image("http://placekitten.com/700/500", "Multiple cute kittens")
