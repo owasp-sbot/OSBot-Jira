9542122f0dbf194bf8ad1067282ada5ffa50c9fe
Dinis Cruz <dinis.cruz@owasp.org>
2019-08-04 11:53:31 +0100

    Added demo data and parser for OSBot GraphDB

---
+++ OSBot-GraphDB/osbot_graphdb/Demo_Data.py
@@ -0,0 +1,94 @@
+import csv
+from pbx_gs_python_utils.utils.Files import Files
+
+
+class Demo_Data:
+    def __init__(self):
+        self.data_folder = Files.path_combine(__file__,'../../db/csv-data')
+
+    # helper methods
+    def file_lines(self, title):
+        lines = []
+        file_path = self.file_path(title)
+        with open(file_path) as csvDataFile:
+            csvReader = csv.reader(csvDataFile)
+            for row in csvReader:
+                lines.append(row)
+        return lines
+
+    def file_path(self, title):
+        return Files.path_combine(self.data_folder, title)
+
+    def file_data(self, title, header_row, row_count):
+        data = []
+        index_start = header_row  + 1
+        index_end   = index_start + row_count
+        lines = self.file_lines(title)
+        headers = lines[header_row]
+        # print(lines[index_start: index_end])
+        # return
+
+        for line in lines[index_start: index_end]:
+            item={}
+            for index, cell in enumerate(line):
+                header = headers[index]
+                if header:
+                    item[header] = cell
+            data.append(item)
+
+        return data
+
+    # data sets
+    def dataset__Sample_Database_extracts__HR_Database(self):
+        return self.file_data('1.Sample Database extracts.csv', 1, 6)
+
+    def dataset__Sample_Database_extracts__Sunways_application_user_extract(self):
+        return self.file_data('1.Sample Database extracts.csv', 10, 6)
+
+    def dataset__People_Role_Reporting_line(self):
+        return self.file_data('2.People, Role, Reporting line.csv', 0, 6)
+
+    def dataset__Role_Team_Function_Business(self):
+        return self.file_data('3.Role, Team, Function, Business.csv', 0, 6)
+
+    def dataset__Device_Person_Account_Application__by_Device(self):
+        return self.file_data('4.Device, Person, Account, Application.csv', 0, 6)
+
+    def dataset__Device_Person_Account_Application__by_Person(self):
+        return self.file_data('4.Device, Person, Account, Application.csv', 9, 6)
+
+    def dataset__Device_Person_Account_Application__by_Account(self):
+        return self.file_data('4.Device, Person, Account, Application.csv', 17, 6)
+
+    def dataset__Business_Asset(self):
+        return self.file_data('5.Business Asset.csv', 0, 6)
+
+    def dataset__Device_Detections(self):
+        data = self.file_data('6.Device, Detections.csv', 0, 28)
+        del data[6:8]           # remove the headers in these locations since
+        del data[12:14]         # they don't represent new types of mappings
+        del data[15:17]         # which means we can reuse row's 1 headers
+        del data[17:19]
+        return data
+
+    def dataset__Account_Detections_2FA(self):
+        data = self.file_data('7.Account, Detections, 2FA.csv', 0, 30)
+        del data[22:24]
+        del data[14:16]
+        del data[6:8]
+        return data
+
+    def dataset__Control_Capabilities_Role_People(self):
+        return self.file_data('8.Control Capabilities, Role, People.csv', 0, 30)
+
+    def dataset__Impacts(self):
+        return self.file_data('9.Impacts.csv', 0, 75)
+
+    def dataset__Impact_Vulnerability_Incident_Fact_Control(self):
+        return self.file_data('10.Impact, Vulnerability, Incident Fact, Control.csv', 0, 24)
+
+    def dataset__Incident(self):
+        return self.file_data('11.Incident.csv', 0, 40)
+
+    def dataset__Vulnerability_Risk_Role(self):
+        return self.file_data('12.Vulnerability, Risk, Role.csv', 0, 11)
---
+++ OSBot-GraphDB/tests/test_Demo_Data.py
@@ -0,0 +1,79 @@
+from unittest import TestCase
+
+from pbx_gs_python_utils.utils.Dev import Dev
+from pbx_gs_python_utils.utils.Files import Files
+
+from osbot_graphdb.Demo_Data import Demo_Data
+
+
+class test_Demo_Data(TestCase):
+
+    def setUp(self):
+        self.demo_data = Demo_Data()
+        self.file_name = '1.Sample Database extracts.csv'
+        self.result    = None
+
+    def tearDown(self):
+        if self.result is not None:
+            Dev.pprint(self.result)
+
+    def test__init__(self):
+        assert Files.folder_exists(self.demo_data.data_folder) is True
+
+    # helper methods
+
+    def test_file_lines(self):
+        assert len(self.demo_data.file_lines(self.file_name)) == 17
+
+    def test_file_path(self):
+        assert Files.exists(self.demo_data.file_path(self.file_name))
+
+    # data sets
+    def test_dataset__Sample_Database_extracts__HR_Database(self):
+        assert len(self.demo_data.dataset__Sample_Database_extracts__HR_Database()) == 6
+
+    def test_dataset__Sample_Database_extracts__Sunways_application_user_extract(self):
+        assert len(self.demo_data.dataset__Sample_Database_extracts__Sunways_application_user_extract()) == 6
+
+    def test_dataset__People_Role_Reporting_line(self):
+        assert len(self.demo_data.dataset__People_Role_Reporting_line()) == 6
+
+    def test_dataset__Role_Team_Function_Business(self):
+        assert len(self.demo_data.dataset__Role_Team_Function_Business()) == 6
+
+    def test_dataset__Device_Person_Account_Application__by_Device(self):
+        assert len(self.demo_data.dataset__Device_Person_Account_Application__by_Device()) == 6
+
+    def test_dataset__Device_Person_Account_Application__by_Person(self):
+        assert len(self.demo_data.dataset__Device_Person_Account_Application__by_Person()) == 6
+
+    def test_dataset__Device_Person_Account_Application__by_Account(self):
+        assert len(self.demo_data.dataset__Device_Person_Account_Application__by_Account()) == 6
+
+    def test_dataset__Business_Asset(self):
+        assert len(self.demo_data.dataset__Business_Asset()) == 6
+
+    def test_dataset__Device_Detections(self):
+        assert len(self.demo_data.dataset__Device_Detections()) == 19
+
+    def test_dataset__Account_Detections_2FA(self):
+        assert len(self.demo_data.dataset__Account_Detections_2FA()) == 24
+
+    def test_dataset__Control_Capabilities_Role_People(self):
+        assert len(self.demo_data.dataset__Control_Capabilities_Role_People()) == 24
+
+    def test_dataset__Impacts(self):
+        assert len(self.demo_data.dataset__Impacts()) == 72
+
+    def test_dataset__Impact_Vulnerability_Incident_Fact_Control(self):
+        assert len(self.demo_data.dataset__Impact_Vulnerability_Incident_Fact_Control()) == 24
+
+    def test_dataset__Incident(self):
+        assert len(self.demo_data.dataset__Incident()) == 40
+
+    def test_dataset__Vulnerability_Risk_Role(self):
+        assert len(self.demo_data.dataset__Vulnerability_Risk_Role()) == 11
+
+        # print('-----')
+        # for item in self.demo_data.dataset__Account_Detections_2FA():
+        #     print(item.get('edge_2'))
