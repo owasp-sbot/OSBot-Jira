280ff8c9aa393457af983655a8a36525689a04f2
Dinis Cruz <dinis.cruz@owasp.org>
2020-02-19 21:04:53 +0000

    minor fix

--- osbot_jira/lambdas/on_jira_change.py
+++ osbot_jira/lambdas/on_jira_change.py
@@ -1,21 +1,20 @@
 import json
 
-from gw_bot.helpers.Lambda_Helpers import log_to_elk
 from osbot_aws.Dependencies import load_dependencies
-
 
 def run(event, context):
     load_dependencies('requests,elastic')
+    from gw_bot.helpers.Lambda_Helpers import log_to_elk
     from osbot_jira.api.jira_server.API_Jira_To_Elastic import API_Jira_To_Elastic
     try:
         data       = json.loads(event.get('body'))
         event_type = data.get('webhookEvent')
         user_id    = event.get('queryStringParameters').get('user_id')
-        message = API_Jira_To_Elastic().handle_event(event_type, user_id, data)
+        message    = API_Jira_To_Elastic().handle_event(event_type, user_id, data)
         status_code = 200
     except Exception as error:
         message = f'error in on_jira_change: {error}'
-        log_to_elk(message, data, index='elastic_logs', level='error')
+        log_to_elk(message, f"{event.get('body')}", index='elastic_logs', level='error')
         status_code = 200                                                   # don't return error 500 or Jira will keep sending it
 
 
